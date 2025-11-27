a598e1584a3549b701adfbdda1eb202ef8773ae1
Moon YuBin <135791086+YUBIN-githubb@users.noreply.github.com>
2025-11-27 13:16:48 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    odd = 0
+    even = 0
+    for i in range (0,len(num_list),2):
+        odd += num_list[i]
+    for i in range (1,len(num_list),2):
+        even += num_list[i]
+    if odd > even:
+        return odd
+    elif odd < even:
+        return even
+    else:
+        return odd
